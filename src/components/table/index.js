77c8676863ced676b6687806478501a0d34fd840
Ta1grr <taile602@gmail.com>
2020-03-07 22:46:12 -0600

    implemented color coding based on type

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Table } from "reactstrap";
+import { Table, Container } from "reactstrap";
 import "./index.css";
 import sampleLogs from "../../SKUNinja-sample-logs.json";
 
@@ -14,30 +14,36 @@ class TableFormat extends Component {
 
   render() {
     return (
-      <Table bordered>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Date / Time</th>
-            <th>Subject</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.sampleLog.map(log => (
+      <Container>
+        <Table bordered className="tableFormat">
+          <thead>
             <tr>
-              <th scope="row">{log.type}</th>
-              <td>{log.created}</td>
-              <td>{log.subject}</td>
+              <th>Type</th>
+              <th>Date / Time</th>
+              <th>Subject</th>
             </tr>
-          ))}
-          {/* <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr> */}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {this.state.sampleLog.map(log => (
+              <tr
+                className={
+                  log.type === "1" ? "row1" : log.type === "2" ? "row2" : "row3"
+                }
+              >
+                <th scope="row">{log.type}</th>
+                <td>{log.created}</td>
+                <td>{log.subject}</td>
+              </tr>
+            ))}
+            {/* <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr> */}
+          </tbody>
+        </Table>
+      </Container>
     );
   }
 }
